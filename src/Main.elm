11e6c3540163954654cfe62f42b2898800953537
Christian Ekrem <christianekrem@gmail.com>
2025-09-26 10:16:53 +0200

    v1: not pretty, but ish working

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,7 @@ import Dict
 import Html exposing (Html)
 import Html.Attributes as Attributes
 import Html.Events as Events
+import Html.Lazy
 import HtmlHelpers
 
 
@@ -54,6 +55,17 @@ type Dividend
     = Dividend
 
 
+parseDividend : String -> Maybe (EquationNumber Dividend)
+parseDividend input =
+    if input |> String.isEmpty then
+        Just (EquationNumber 0)
+
+    else
+        input
+            |> String.toInt
+            |> Maybe.map EquationNumber
+
+
 toDividend : Int -> EquationNumber Dividend
 toDividend int =
     EquationNumber int
@@ -75,6 +87,17 @@ type Divisor
 toDivisor : Int -> EquationNumber Divisor
 toDivisor int =
     EquationNumber int
+
+
+parseDivisor : String -> Maybe (EquationNumber Divisor)
+parseDivisor input =
+    if input |> String.isEmpty then
+        Just (EquationNumber 0)
+
+    else
+        input
+            |> String.toInt
+            |> Maybe.map EquationNumber
 
 
 type QuotientPart
@@ -198,6 +221,8 @@ type Msg
     = Noop
     | NextStep
     | PrevStep
+    | SetDividend String
+    | SetDivisor String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -211,6 +236,22 @@ update msg model =
 
         PrevStep ->
             ( { model | activeStep = max (model.activeStep - 1) 0 }, Cmd.none )
+
+        SetDividend input ->
+            case input |> parseDividend of
+                Just d ->
+                    ( { model | dividend = d }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        SetDivisor input ->
+            case input |> parseDivisor of
+                Just d ->
+                    ( { model | divisor = d }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
 
 
 
@@ -228,54 +269,106 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    let
-        ( steps, numberOfSteps ) =
-            model |> equationSteps
-    in
-    Html.div
-        [ Attributes.style "display" "flex"
-        , Attributes.style "flex-direction" "column"
-        , Attributes.style "align-items" "center"
-        , Attributes.style "justify-content" "center"
-        , Attributes.style "font-family" "monospace"
-        , Attributes.style "font-size" "6vmin"
-        ]
-        [ Html.h1 [] [ Html.text "Elm Division" ]
-        , Html.div []
-            [ Html.button [ Events.onClick PrevStep, Attributes.disabled (model.activeStep == 0) ] [ Html.text "<" ]
-            , Html.button [ Events.onClick NextStep, Attributes.disabled (model.activeStep == numberOfSteps) ] [ Html.text ">" ]
-            ]
-        , viewEquationWrapper <|
-            Html.text (viewEquation model)
-                :: (steps
-                        |> List.indexedMap
-                            (\index st ->
-                                Html.div [ Attributes.style "color" (color index) ]
-                                    [ Html.div []
-                                        [ Html.div [] [ textSpacer (index + 1), Html.text (st.dividendWithRemainder |> toString) ]
-                                        , Html.div [] [ textSpacer index, Html.text (st.product |> toString |> String.append "-") ]
-                                        , Html.div [] [ textSpacer index, Html.text (st.remainder |> toString |> String.append "= ") ]
-                                        ]
-                                    ]
-                            )
-                   )
+    Html.Lazy.lazy3
+        (\dividend divisor activeStep ->
+            let
+                ( steps, numberOfSteps ) =
+                    model |> equationSteps
 
-        --  , viewStepExplanation steps
-        ]
+                activeSteps =
+                    steps |> List.take activeStep
+            in
+            Html.div
+                [ Attributes.style "display" "flex"
+                , Attributes.style "flex-direction" "column"
+                , Attributes.style "height" "100vh"
+                , Attributes.style "width" "100vw"
+                , Attributes.style "box-sizing" "border-box"
+                , Attributes.style "padding" "2em 0"
+                , Attributes.style "overflow-y" "auto"
+                , Attributes.style "align-items" "center"
+                , Attributes.style "justify-content" "flex-start"
+                , Attributes.style "font-family" "sans-serif"
+                , Attributes.style "font-size" "6vmin"
+                ]
+                [ Html.h3 [ Attributes.style "margin" "0" ] [ Html.text "Elm Division" ]
+                , Html.div []
+                    [ Html.input [ Attributes.value (dividend |> toString), Events.onInput SetDividend ] []
+                    , Html.input [ Attributes.value (divisor |> toString), Events.onInput SetDivisor ] []
+                    ]
+                , Html.div []
+                    [ Html.button [ Events.onClick PrevStep, Attributes.disabled (model.activeStep == 0) ] [ Html.text "<" ]
+                    , Html.button [ Events.onClick NextStep, Attributes.disabled (model.activeStep == numberOfSteps) ] [ Html.text ">" ]
+                    ]
+                , viewEquation { dividend = dividend, divisor = divisor, steps = activeSteps }
+                , viewStepExplanation activeSteps
+                ]
+        )
+        model.dividend
+        model.divisor
+        model.activeStep
 
 
-viewEquationWrapper : List (Html msg) -> Html msg
-viewEquationWrapper content =
+mathGridWrapper : List (Html msg) -> Html msg
+mathGridWrapper content =
     Html.div
         [ Attributes.style "background-image"
-            "linear-gradient(to right, grey 1px, transparent 1px), linear-gradient(to bottom, grey 1px, transparent 1px)"
-
-        -- TODO: Align
-        , Attributes.style "background-size" "0.6em 1.18em, 0.6em 1.18em"
+            (String.join ","
+                [ "linear-gradient(to right, transparent calc(100% - 0.5px), grey 100%)"
+                , "linear-gradient(to bottom, transparent calc(100% - 0.5px), grey 100%)"
+                ]
+            )
+        , Attributes.style "background-size" "1ch calc(1em - 1px), 1ch calc(1em - 1px)"
+        , Attributes.style "background-position" "0.5px 0.5px"
+        , Attributes.style "background-origin" "content-box"
+        , Attributes.style "background-clip" "content-box"
         , Attributes.style "background-repeat" "repeat, repeat"
-        , Attributes.style "text-align" "left"
+
+        -- typography
+        , Attributes.style "font-family"
+            "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace"
+        , Attributes.style "font-variant-numeric" "tabular-nums"
+        , Attributes.style "font-feature-settings" "\"tnum\" 1"
+        , Attributes.style "line-height" "1"
+        , Attributes.style "letter-spacing" "0"
+
+        -- avoid shifting the grid
+        , Attributes.style "padding" "0"
+        , Attributes.style "margin" "1em"
+        , Attributes.style "outline" "1px solid grey"
         ]
         content
+
+
+viewEquation : { a | dividend : EquationNumber Dividend, divisor : EquationNumber Divisor, steps : List Step } -> Html msg
+viewEquation { dividend, divisor, steps } =
+    mathGridWrapper
+        [ textSpacer 1
+        , Html.text (equationText { dividend = dividend, divisor = divisor })
+        , (steps
+            |> mapToColoredSpans
+                (\st ->
+                    [ Html.text (st.quotient |> toString) ]
+                )
+                []
+          )
+            |> HtmlHelpers.wrapToSingleNode
+        , (steps
+            |> mapToColoredDivsIndexed
+                (\index st ->
+                    [ Html.div []
+                        [ HtmlHelpers.when (index > 1) (Html.div [] [ textSpacer (index + 1), Html.text (st.dividendWithRemainder |> toString) ])
+                        , Html.div [] [ textSpacer index, Html.text (st.product |> toString |> String.append "-") ]
+
+                        -- TODO: Append next number "drawn down" (foldr cons perhaps?)
+                        , Html.div [] [ textSpacer index, Html.text ("= " ++ (st.remainder |> toString)) ]
+                        ]
+                    ]
+                )
+                []
+          )
+            |> HtmlHelpers.wrapToSingleNode
+        ]
 
 
 equationColors : Array.Array String
@@ -306,19 +399,30 @@ equationColors =
 
 color : Int -> String
 color int =
-    equationColors |> Array.get int |> Maybe.withDefault "unset"
+    let
+        index =
+            remainderBy (Array.length equationColors) int
+    in
+    equationColors |> Array.get index |> Maybe.withDefault "unset"
+
+
+mapToColoredSpans : (a -> List (Html msg)) -> List (Html.Attribute msg) -> List a -> List (Html msg)
+mapToColoredSpans transform attributes =
+    List.indexedMap (\index entry -> Html.span (Attributes.style "color" (color index) :: attributes) (transform entry))
+
+
+mapToColoredDivsIndexed : (Int -> a -> List (Html msg)) -> List (Html.Attribute msg) -> List a -> List (Html msg)
+mapToColoredDivsIndexed transform attributes =
+    List.indexedMap (\index entry -> Html.div (Attributes.style "color" (color index) :: attributes) (transform index entry))
 
 
 viewStepExplanation : List Step -> Html msg
 viewStepExplanation steps =
     Html.div []
         (steps
-            |> List.indexedMap
-                (\index st ->
-                    Html.div
-                        [ Attributes.style "color" (color index)
-                        , Attributes.style "text-align" "right"
-                        ]
+            |> mapToColoredDivsIndexed
+                (\_ st ->
+                    [ mathGridWrapper
                         [ Html.div []
                             [ Html.text (st.dividendWithRemainder |> toString)
                             , Html.text " : "
@@ -344,9 +448,10 @@ viewStepExplanation steps =
                             [ Html.text "Remainder = "
                             , Html.text (st.remainder |> toString)
                             ]
-                        , Html.hr [] []
                         ]
+                    ]
                 )
+                []
         )
 
 
@@ -355,11 +460,11 @@ textSpacer int =
     "" |> String.padLeft int (Char.fromCode 160) |> Html.text
 
 
-viewEquation :
+equationText :
     { a
         | dividend : EquationNumber Dividend
         , divisor : EquationNumber Divisor
     }
     -> String
-viewEquation { dividend, divisor } =
+equationText { dividend, divisor } =
     (dividend |> toString) ++ " : " ++ (divisor |> toString) ++ " = "
